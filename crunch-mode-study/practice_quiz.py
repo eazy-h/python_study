120853a4c464738bef84a5678ea8235dca84687d
live-jh <livejh7129@gmail.com>
2021-08-01 22:46:46 +0900

    ✚ algorithm_study_day_3

--- crunch-mode-study/practice_quiz.py
+++ crunch-mode-study/practice_quiz.py
@@ -29,3 +29,45 @@ a, b, c =map(int,input().split())
 plus_no = a+b+c
 avg_no = plus_no / 3
 print(plus_no, format(avg_no, '.2f'))
+
+
+
+# Quiz 4.
+# 월 숫자 입력시 계절 출력
+# a = int(input())
+# w = [12, 1, 2]
+# sp = [3, 4, 5]
+# sm = [6, 7, 8]
+# f = [9, 10, 11]
+# if a in w:
+#     print("winter")
+# elif a in sp:
+#     print("spring")
+# elif a in sm:
+#     print("summer")
+# else:
+#     print("fall")
+
+# a=int(input())
+# if a//3==1:
+#     print('spring')
+# elif a//3==2:
+#     print('summer')
+# elif a//3==3:
+#     print('fall')
+# else:
+#     print('winter')
+
+
+#Quiz. 5
+
+#a부터 입력한 문자까지 순서대로 공백 두고 한줄 출력
+# ex) d -> a b c d
+
+# a = ord(input())
+# b = ord('a')
+#
+# while b <= a:
+# 	print(chr(b), end="" if b == a else " ")
+# 	b+=1
+#
